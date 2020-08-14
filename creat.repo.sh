2e7463bda5144d9525f74ea18060b2de2e107be6
LiuKe98 <190110455@pku.edu.cn>
2020-08-14 11:34:41 +0800

    submit creat.repo.sh

--- creat.repo.sh
+++ creat.repo.sh
@@ -33,7 +33,7 @@ git remote add origin git@github.com:LiuKe98/test_repo.git
 ### the first time 推送
 git push -u origin maste
 ####之后推送
-git push  origin maste
+git push  origin master
 ###更改远程名字
 git remote -v
 git remote rename origin test.repo
@@ -41,3 +41,25 @@ git remote -v
 ###删除远程
 git remote rm names
 git remote -v
+###查看分支
+git branch
+###创建分支并切换到分支
+git checkout -b dev.name 
+###创建分支并切换到分支
+git switch -c dev.name
+###创建分支
+git branch dev.name
+###切换分支
+git checkout dev.name
+###切换分支
+git switch dev.name
+###切换到主分支
+git checkout master
+###将dev分支和master分支合并
+git merge dev.name
+###删除dev分支
+git branch -d dev.name
+###推送
+git push origin master
+git log --graph --pretty=oneline --abbrev-commit
+
